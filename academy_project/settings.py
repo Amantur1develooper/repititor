72c617585e1b8e08e2e9b75a030ca90cb94ceb05
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-10-09 22:40:26 +0600

    h3

--- academy_project/settings.py
+++ academy_project/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 # Добавьте настройки для статических файлов и медиа
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
