f05a5ccd6c0d2634aeb72b8b38ef6fee8fdc123c
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-11-03 20:24:35 +0600

    date

--- groups/models.py
+++ groups/models.py
@@ -54,10 +54,35 @@ class Enrollment(models.Model):
     def __str__(self):
         return f"{self.student} в {self.group}"
     
-    def increment_attendance(self):
-        """Увеличивает счетчик посещенных занятий"""
-        self.lessons_attended += 1
-        self.save()
+    
+    def increment_attendance(self, by_user=None, source='single', note: str = ''):
+        """
+        Увеличивает счётчик посещений и пишет запись в AttendanceLog — атомарно.
+        :param by_user: User, кто отметил
+        :param source: 'single' | 'bulk'
+        :param note: необязательная пометка
+        """
+        from .models import AttendanceLog  # локальный импорт, чтобы избежать циклов
+
+        with transaction.atomic():
+            # +1 к посещениям (через F-выражение без гонок)
+            type(self).objects.filter(pk=self.pk).update(lessons_attended=F('lessons_attended') + 1)
+            # перечитать фактическое значение
+            self.refresh_from_db(fields=['lessons_attended'])
+
+            AttendanceLog.objects.create(
+            enrollment=self,
+            group=self.group,
+            marked_by=by_user,
+            source=source,
+            lesson_no_at_time=self.lessons_attended,
+            note=note
+        )
+
+    # def increment_attendance(self):
+    #     """Увеличивает счетчик посещенных занятий"""
+    #     self.lessons_attended += 1
+    #     self.save()
     
     def get_personal_lesson_count(self):
         """Возвращает персональный счетчик занятий студента"""
@@ -83,3 +108,74 @@ class Enrollment(models.Model):
         if current_lesson_in_month == 0 and self.lessons_attended > 0:
             return 100
         return (current_lesson_in_month / self.group.lessons_per_month) * 100
+# groups/models.py
+from django.db import models, transaction
+from django.db.models import F
+from django.utils import timezone
+from django.conf import settings
+
+# ... твои Group, Enrollment и т.д. выше
+
+class AttendanceLog(models.Model):
+    class Source(models.TextChoices):
+        SINGLE = "single", "Индивидуально (кнопка у студента)"
+        BULK = "bulk", "Групповое (кнопка «занятие для всех»)"
+
+    enrollment = models.ForeignKey(
+        'groups.Enrollment',
+        on_delete=models.CASCADE,
+        related_name='attendance_logs',
+        verbose_name='Запись зачисления'
+    )
+    group = models.ForeignKey(
+        'groups.Group',
+        on_delete=models.CASCADE,
+        related_name='attendance_logs',
+        verbose_name='Группа',
+        db_index=True
+    )
+    marked_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True, blank=True,
+        on_delete=models.SET_NULL,
+        related_name='attendance_marked',
+        verbose_name='Кто отметил'
+    )
+    marked_at = models.DateTimeField(
+        default=timezone.now,
+        verbose_name='Когда отмечено',
+        db_index=True
+    )
+    source = models.CharField(
+        max_length=12,
+        choices=Source.choices,
+        default=Source.SINGLE,
+        verbose_name='Источник отметки'
+    )
+    lesson_no_at_time = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Номер занятия у студента на момент отметки'
+    )
+    note = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name='Примечание'
+    )
+
+    class Meta:
+        verbose_name = 'Лог посещаемости'
+        verbose_name_plural = 'Логи посещаемости'
+        ordering = ['-marked_at']
+        indexes = [
+            models.Index(fields=['group', '-marked_at']),
+            models.Index(fields=['enrollment', '-marked_at']),
+        ]
+
+    def __str__(self):
+        sname = getattr(self.enrollment.student, 'full_name', str(self.enrollment.student_id))
+        return f"{sname} @ {self.marked_at:%Y-%m-%d %H:%M} ({self.get_source_display()})"
+
+
+
+# внутри класса Enrollment (groups/models.py)
+from django.db import transaction
--- groups/views.py
+++ groups/views.py
@@ -13,8 +13,8 @@ from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from django.urls import reverse_lazy
 from users.decorators import role_required
-from django.utils.decorators import method_decorator
-from .models import Group, Enrollment
+
+from .models import Group, Enrollment, AttendanceLog
 from dateutil.relativedelta import relativedelta
 
 # @method_decorator(role_required(['admin', 'curator']), name='dispatch')
@@ -43,52 +43,43 @@ class GroupCreateView(LoginRequiredMixin, CreateView):
     form_class = GroupForm
     template_name = 'groups/group_form.html'
     success_url = reverse_lazy('group_list')
-    
+
 class GroupIncrementLessonView(LoginRequiredMixin, View):
     """View для увеличения счетчика занятий группы"""
-    
     def post(self, request, pk):
         group = get_object_or_404(Group, pk=pk)
-        
-        # Увеличиваем счетчики посещений для всех студентов группы
-        enrollments = Enrollment.objects.filter(group=group)
+        enrollments = Enrollment.objects.filter(group=group).select_related('student')
+
         for enrollment in enrollments:
-            enrollment.increment_attendance()
-        
-        messages.success(
-            request, 
-            f'Занятие добавлено для всех студентов группы'
-        )
-        
+            enrollment.increment_attendance(
+                by_user=request.user,
+                source=AttendanceLog.Source.BULK
+            )
+
+        messages.success(request, 'Занятие добавлено для всех студентов группы')
         return redirect('group_detail', pk=group.pk)
+    
+    
 # class GroupIncrementLessonView(LoginRequiredMixin, View):
-#     """View для увеличения счетчика занятий и посещений"""
+#     """View для увеличения счетчика занятий группы"""
     
 #     def post(self, request, pk):
 #         group = get_object_or_404(Group, pk=pk)
-        
-#         # Увеличиваем общее количество занятий в группе
-#         group.lessons_count += 1
-#         group.save()
+
+
         
 #         # Увеличиваем счетчики посещений для всех студентов группы
 #         enrollments = Enrollment.objects.filter(group=group)
 #         for enrollment in enrollments:
 #             enrollment.increment_attendance()
         
-#         # Проверяем, нужно ли проверять оплату
-#         if group.should_check_payment():
-#             messages.warning(
-#                 request, 
-#                 f'Группа завершила {group.lessons_count} занятий! Проверьте оплату за {group.get_next_month()} месяц.'
-#             )
-#         else:
-#             messages.success(
-#                 request, 
-#                 f'Занятие добавлено. Всего проведено: {group.lessons_count}'
-#             )
+#         messages.success(
+#             request, 
+#             f'Занятие добавлено для всех студентов группы'
+#         )
         
 #         return redirect('group_detail', pk=group.pk)
+
 # @method_decorator(role_required(['admin']), name='dispatch')
 class GroupUpdateView(LoginRequiredMixin, UpdateView):
     model = Group
@@ -167,228 +158,14 @@ class GroupDetailView(LoginRequiredMixin, DetailView):
         context['partial_count'] = partial_count
         context['completed_count'] = completed_count
         context['not_paid_count'] = not_paid_count
-
+        context['attendance_logs'] = (
+    AttendanceLog.objects
+    .filter(group=group)
+    .select_related('enrollment__student', 'marked_by')
+    .order_by('-marked_at')[:100]
+)
         return context
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
 
-#         enrollments = group.enrollment_set.all().select_related('student')
-        
-        
-        
-      
-#         enrollments_with_payments = []
-        
-#         for enrollment in enrollments:
-#             next_personal_month = enrollment.get_next_personal_month()
-#             payment_status = Payment.get_student_payment_status_by_month_number(
-#                 enrollment.student,
-#                 group,
-#                 next_personal_month
-#             )
-            
-#             enrollments_with_payments.append({
-#                 'enrollment': enrollment,
-#                 'payment_status': payment_status,
-#                 'next_personal_month': next_personal_month,
-#                 'should_check_payment': enrollment.should_check_personal_payment(),
-#                 'progress_percent': enrollment.get_personal_progress_percent()
-#             })
-
-#         total_count = len(enrollments_with_payments)
-#         paid_count = sum(1 for i in enrollments_with_payments if i['payment_status']['is_fully_paid'])
-#         partial_count = sum(1 for i in enrollments_with_payments if i['payment_status']['is_partially_paid'])
-#         completed_count = sum(1 for i in enrollments_with_payments if i['should_check_payment'])
-#         not_paid_count = sum(
-#             1 for i in enrollments_with_payments 
-#             if i['should_check_payment'] and not i['payment_status']['is_fully_paid']
-#         )
-
-#         context['enrollments_with_payments'] = enrollments_with_payments
-#         context['total_count'] = total_count
-#         context['paid_count'] = paid_count
-#         context['partial_count'] = partial_count
-#         context['completed_count'] = completed_count
-#         context['not_paid_count'] = not_paid_count
-
-#         return context
-
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
-
-#         enrollments = group.enrollment_set.all().select_related('student')
-
-#         enrollments_with_payments = []
-#         for enrollment in enrollments:
-#             next_personal_month = enrollment.get_next_personal_month()
-#             payment_status = Payment.get_student_payment_status_by_month_number(
-#                 enrollment.student,
-#                 group,
-#                 next_personal_month
-#             )
-#             enrollments_with_payments.append({
-#                 'enrollment': enrollment,
-#                 'payment_status': payment_status,
-#                 'next_personal_month': next_personal_month,
-#                 'should_check_payment': enrollment.should_check_personal_payment(),
-#                 'progress_percent': enrollment.get_personal_progress_percent()
-#             })
-
-#         total_count = len(enrollments_with_payments)
-#         paid_count = sum(1 for i in enrollments_with_payments if i['payment_status']['is_fully_paid'])
-#         partial_count = sum(1 for i in enrollments_with_payments if i['payment_status']['is_partially_paid'])
-#         context['enrollments_with_payments'] = enrollments_with_payments
-#         context['total_count'] = total_count
-#         context['paid_count'] = paid_count
-#         context['partial_count'] = partial_count
-
-#         return context
-
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
-
-#         # Получаем все зачисления студентов
-#         enrollments = group.enrollment_set.all().select_related('student')
-
-#         # Формируем список с информацией о платежах
-#         enrollments_with_payments = []
-#         for enrollment in enrollments:
-#             next_personal_month = enrollment.get_next_personal_month()
-#             payment_status = Payment.get_student_payment_status_by_month_number(
-#                 enrollment.student,
-#                 group,
-#                 next_personal_month
-#             )
-
-#             enrollments_with_payments.append({
-#                 'enrollment': enrollment,
-#                 'payment_status': payment_status,
-#                 'next_personal_month': next_personal_month,
-#                 'should_check_payment': enrollment.should_check_personal_payment(),
-#                 'progress_percent': enrollment.get_personal_progress_percent()
-#             })
-
-#         # ✅ Подсчёт количества оплативших и общего числа
-#         total_count = len(enrollments_with_payments)
-#         paid_count = sum(
-#             1 for item in enrollments_with_payments 
-#             if item['payment_status']['is_fully_paid']
-#         )
-
-#         # Добавляем данные в контекст
-#         context['enrollments_with_payments'] = enrollments_with_payments
-#         context['total_count'] = total_count
-#         context['paid_count'] = paid_count
-
-#         return context
-
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-        
-#         group = self.object
-        
-        
-        
-#         # Получаем всех студентов группы
-#         enrollments = group.enrollment_set.all().select_related('student')
-        
-#         # Подготавливаем данные о платежах для каждого студента
-#         enrollments_with_payments = []
-        
-#         for enrollment in enrollments:
-#             # Определяем следующий персональный месяц для студента
-#             next_personal_month = enrollment.get_next_personal_month()
-            
-#             # Получаем статус оплаты за следующий месяц
-#             payment_status = Payment.get_student_payment_status_by_month_number(
-#                 enrollment.student, 
-#                 group, 
-#                 next_personal_month
-#             )
-            
-#             enrollments_with_payments.append({
-#                 'enrollment': enrollment,
-#                 'payment_status': payment_status,
-#                 'next_personal_month': next_personal_month,
-#                 'should_check_payment': enrollment.should_check_personal_payment(),
-#                 'progress_percent': enrollment.get_personal_progress_percent()
-#             })
-        
-#         # Подсчёт количества оплативших и общего числа
-#         enrollments_with_payments = context['enrollments_with_payments']
-#         total_count = len(enrollments_with_payments)
-#         paid_count = sum(
-#             1 for item in enrollments_with_payments 
-#             if item['payment_status']['is_fully_paid']
-#         )
-        
-#         context['total_count'] = total_count
-#         context['paid_count'] = paid_count
-#         context['enrollments_with_payments'] = enrollments_with_payments
-        
-        
-#         return context
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
-        
-#         # Определяем месяц, за который проверяем оплату
-#         check_payment_month = self.get_payment_check_month(group)
-        
-#         # Получаем всех студентов группы
-#         enrollments = group.enrollment_set.all().select_related('student')
-        
-#         # Подготавливаем данные о платежах для каждого студента
-#         enrollments_with_payments = []
-        
-#         for enrollment in enrollments:
-#             payment_status = Payment.get_student_payment_status(
-#                 enrollment.student, 
-#                 group, 
-#                 check_payment_month
-#             )
-            
-#             enrollments_with_payments.append({
-#                 'enrollment': enrollment,
-#                 'payment_status': payment_status,
-#                 'check_month': check_payment_month
-#             })
-        
-#         # Передаем данные в контекст
-#         context['enrollments_with_payments'] = enrollments_with_payments
-#         context['should_check_payment'] = group.should_check_payment()
-#         context['next_month'] = group.get_next_month()
-#         context['check_payment_month'] = check_payment_month
-        
-#         return context
     
     def get_payment_check_month(self, group):
         """
@@ -406,148 +183,22 @@ class GroupDetailView(LoginRequiredMixin, DetailView):
     
     
     
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
     
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
-        
-#         # Получаем всех студентов группы с информацией о зачислениях
-#         enrollments = group.enrollment_set.all().select_related('student')
-        
-#         # Добавляем информацию о платежах каждого студента
-#         enrollments_with_payments = []
-#         next_month = group.get_next_month()
-        
-#         for enrollment in enrollments:
-#             # Проверяем, оплачен ли следующий месяц
-#             is_paid = PaymentDate.get_student_payment_status(
-#                 enrollment.student, 
-#                 group, 
-#                 next_month
-#             )
-            
-#             # Сумма долга: стоимость месяца, если не оплачено
-#             amount_due = group.monthly_price if not is_paid else 0
-            
-#             enrollments_with_payments.append({
-#                 'enrollment': enrollment,
-#                 'is_paid': is_paid,
-#                 'amount_due': amount_due
-#             })
-        
-#         # Передаем все необходимые переменные в контекст
-#         context['enrollments_with_payments'] = enrollments_with_payments
-#         context['should_check_payment'] = group.should_check_payment()
-#         context['next_month'] = next_month
-#         context['enrollments'] = enrollments  # Добавляем обычные enrollments для совместимости
-        
-#         return context
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
-        
-#         # Добавляем информацию о платежах каждого студента
-#         enrollments_with_payments = []
-#         for enrollment in group.enrollment_set.all():
-#             total_paid = Payment.objects.filter(
-#                 student=enrollment.student,
-#                 group=group
-#             ).aggregate(total=Sum('amount'))['total'] or 0
-            
-#             # Проверяем, хватает ли средств для оплаты текущего месяца
-#             current_month = group.get_current_month()
-#             is_paid = total_paid >= (group.monthly_price * current_month)
-            
-#             enrollments_with_payments.append({
-#                 'enrollments': enrollment,
-#                 'total_paid': total_paid,
-#                 'is_paid': is_paid,
-#                 'amount_due': max(0, (group.monthly_price * current_month) - total_paid)
-#             })
-        
-#         context['enrollments_with_payments'] = enrollments_with_payments
-#         return context
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
-
-#         student_payment_status = {}
-#         next_month = group.get_next_month()
-
-#         for enrollment in group.enrollment_set.all():
-#             is_paid = group.get_student_payment_status(
-#                 enrollment.student,
-#                 next_month
-#             )
-#             student_payment_status[enrollment.student.id] = {
-#                 'is_paid': is_paid,
-#                 'enrollment': enrollment
-#             }
-#         enrollments = Enrollment.objects.filter(group=group).select_related('student')
-#         context['enrollments'] = enrollments
-#         context['student_payment_status'] = student_payment_status
-#         context['next_month'] = next_month
-#         return context
-
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         group = self.object
-        
-#         # Получаем всех студентов группы с информацией о зачислениях
-#         enrollments = Enrollment.objects.filter(group=group).select_related('student')
-        
-#         # Подготавливаем данные для отображения статуса оплаты
-#         student_status = {}
-#         for enrollment in enrollments:
-#             status = group.get_student_payment_status(enrollment.student)
-#             student_status[enrollment.student.id] = {
-#                 'status': status,
-#                 'enrollment': enrollment
-#             }
-        
-#         context['student_status'] = student_status
-#         context['enrollments'] = enrollments
-#         context['should_check_payment'] = group.should_check_payment()
-#         context['next_month'] = group.get_next_month()
-        
-#         return context
-
 class MarkStudentAttendanceView(LoginRequiredMixin, View):
     """View для отметки посещения конкретного студента"""
-    
     def post(self, request, group_id, student_id):
         group = get_object_or_404(Group, pk=group_id)
-        enrollment = get_object_or_404(
-            Enrollment, 
-            group=group, 
-            student_id=student_id
+        enrollment = get_object_or_404(Enrollment, group=group, student_id=student_id)
+
+        enrollment.increment_attendance(
+            by_user=request.user,
+            source=AttendanceLog.Source.SINGLE
         )
-        
-        enrollment.increment_attendance()
+
         messages.success(
-            request, 
+            request,
             f'Посещение отмечено для {enrollment.student.full_name}. Всего: {enrollment.lessons_attended}'
         )
-        
         return redirect('group_detail', pk=group_id)
 # class MarkStudentAttendanceView(LoginRequiredMixin, View):
 #     """View для отметки посещения конкретного студента"""
@@ -568,7 +219,4 @@ class MarkStudentAttendanceView(LoginRequiredMixin, View):
         
 #         return redirect('group_detail', pk=group_id)
 
-# class GroupDetailView(LoginRequiredMixin, DetailView):
-#     model = Group
-#     template_name = 'groups/group_detail.html'
-#     context_object_name = 'group'
+
